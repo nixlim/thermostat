dd9c7247cf484bd0bd43539c8f0d5eba5fac7dd1
Igor Ryabchuk <riabtchuk@gmail.com>
2018-05-17 17:07:37 +0100

    adds an API call

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
   $('.slider').val(thermostat.temperature);
   $('.temperature-display').html(thermostat.temperature);
 
+  $.get('http://api.openweathermap.org/data/2.5/forecast?id=2643743&units=metric&APPID=9b17508b9c89e36716d8257b41b462bb', function (response) {
+    console.log(response);
+  });
+
   function upOrDown (value) {
     if (value - thermostat.temperature > 0) {
       try { thermostat.temperatureUp(value-thermostat.temperature); }
